cb1e107a2d795fb02a17739ccb4b4e53554991bb
Toms Fridrihsons <fridrihsonst@gmail.com>
2020-06-10 15:23:40 +0300

    contact-info

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index/8+0.2}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/15+0.2}s`;
 
 
       }
