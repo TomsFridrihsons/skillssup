ef7305a62cf5545c88e60ea354b7aacf8d16904e
Toms Fridrihsons <65284835+TomsFridrihsons@users.noreply.github.com>
2020-06-10 12:42:46 +0300

    Pievienoju jau esošos failus

---
+++ app.js
@@ -0,0 +1,38 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  // const btn=document.querySelectorAll(".burger");
+  const viss = document.querySelector('.viss');
+
+
+  //Toggle nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    viss.classList.toggle('blur-active');
+
+
+    //animate navLinks
+    navLinks.forEach((link, index) => {
+
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/8+0.2}s`;
+
+
+      }
+    });
+
+
+    //Burger animācija
+    burger.classList.toggle('toggle');
+
+    //blur efekts
+
+
+  })
+}
+
+
+navSlide();
